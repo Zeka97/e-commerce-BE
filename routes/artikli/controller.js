297fcc0b612eca55ad9c203ba194c8694fc28daf
Amel Zekovic <amelzekovic123@hotmail.com>
2023-12-10 21:20:41 +0100

    last commit

--- routes/artikli/controller.js
+++ routes/artikli/controller.js
@@ -3,6 +3,7 @@ import * as service from "./service.js";
 export const getAllArticles = async (req, res, next) => {
   try {
     const params = req.query;
+    console.log(params);
     const result = await service.getAllArticles(params);
     return res.status(200).send(result);
   } catch (e) {
